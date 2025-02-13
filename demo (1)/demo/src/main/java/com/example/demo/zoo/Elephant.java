0893c6a2084f9ed8de6c16605ab34dbe0a6a1c82
LevMikhaylov <152269424+LevMikhaylov@users.noreply.github.com>
2025-02-13 18:26:59 +0300

    Update Elephant.java

--- demo (1)/demo/src/main/java/com/example/demo/zoo/Elephant.java
+++ demo (1)/demo/src/main/java/com/example/demo/zoo/Elephant.java
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class Elephant implements Animal{
     @Override
-    public String makeSound(){
-        return "Тру-у-у!";
+    public void makeSound(){
+        System.out.println("Тру-у-у!");
     }
     @Value("${elephant.name}")
     private String name;
