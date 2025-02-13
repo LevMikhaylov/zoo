ccb7a720fe8210d95f869cbebe80a6b24abd0465
LevMikhaylov <152269424+LevMikhaylov@users.noreply.github.com>
2025-02-13 18:11:48 +0300

    Update Zoo.java

--- demo (1)/demo/src/main/java/com/example/demo/zoo/Zoo.java
+++ demo (1)/demo/src/main/java/com/example/demo/zoo/Zoo.java
@@ -1,19 +1,30 @@
 package com.example.demo.zoo;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Zoo {
+    private List<Animal> animals;
     private Animal favoriteAnimal;
-
     @Autowired
-    public Zoo(@Qualifier("duck") Animal favoriteAnimal) {
+    public Zoo(List<Animal> animals, @Qualifier("duck") Animal favoriteAnimal) {
+        this.animals = animals;
         this.favoriteAnimal = favoriteAnimal;
     }
-
+    public void displayAnimalSounds() {
+        System.out.println("В зоопарке есть следующие животные:");
+        for (Animal animal : animals) {
+            System.out.println(animal.getName() + " говорит: ");
+            animal.makeSound();
+        }
+    }
     public void displayFavoriteAnimalSound() {
         System.out.println("В зоопарке есть любимый питомец: " + favoriteAnimal.getName());
-        System.out.println(favoriteAnimal.getName() + " говорит: " + favoriteAnimal.makeSound());
+        System.out.println(favoriteAnimal.getName() + " говорит: ");
+        favoriteAnimal.makeSound();
     }
 }
+
